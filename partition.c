49b88204435082b5d44c0ae8e79000d4cabe99a2
Kyle Liao <thekyleliao@gmail.com>
2025-10-28 08:43:43 -0400

    1

--- partition.c
+++ partition.c
@@ -5,7 +5,7 @@
 
 static void printPartition(int * arr, int length) {
     int ind;
-    for (ind = 0; ind < length; ind ++) {
+    for (ind = 0; ind < length-1; ind ++) {
         printf("%d + ", arr[ind]);
     }
     printf("%d\n", arr[length - 1]);
